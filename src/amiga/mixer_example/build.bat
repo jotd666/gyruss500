cc10286be06038c8334603163c67c685e4d57396
Jean-François Fabre <jotd666@gmail.com>
2025-06-01 12:44:24 +0200

    example working but not mixed!

--- src/amiga/mixer_example/build.bat
+++ src/amiga/mixer_example/build.bat
@@ -1,3 +1,6 @@
+
+cd /D %~pd0
+echo %CD%
 m68k-amigaos-as -I.. -I../mit_includes -o obj/minimal_mixer.o minimal_mixer.68k
 m68k-amigaos-as -o obj/samples.o samples.68k
 m68k-amigaos-as -I.. -I../mit_includes -o obj/mixer.o ../mixer.68k

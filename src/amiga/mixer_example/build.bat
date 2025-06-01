be7c7dc113c395bf640326535d98d47b6bf0be57
Jean-François Fabre <jotd666@gmail.com>
2025-06-01 12:32:57 +0200

    mixer example linking

--- src/amiga/mixer_example/build.bat
+++ src/amiga/mixer_example/build.bat
@@ -1,8 +1,7 @@
 m68k-amigaos-as -I.. -I../mit_includes -o obj/minimal_mixer.o minimal_mixer.68k
 m68k-amigaos-as -o obj/samples.o samples.68k
 m68k-amigaos-as -I.. -I../mit_includes -o obj/mixer.o ../mixer.68k
-m68k-amigaos-as -I.. -I../mit_includes -o obj/converter.o ../converter.68k
 
-m68k-amigaos-ld -o mixer_test obj/*.o
+m68k-amigaos-ld -o mixer_test obj/minimal_mixer.o obj/samples.o obj/mixer.o
 pause
 
